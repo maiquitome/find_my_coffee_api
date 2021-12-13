e9972eea99c83e02396ba8c24961a31313bc524e
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-12-13 19:07:19 -0300

    :bug: change options to opinion

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,6 @@
 class Rating < ApplicationRecord
   belongs_to :store
-  validate_presence_of :value, :options, :user_name
+  validates_presence_of :value, :opinion, :user_name
 end
+
+# irb> Rating.create(store: Store.first, value: 4, opinion: "muito bom o lugar", user_name: "Maiqui")
